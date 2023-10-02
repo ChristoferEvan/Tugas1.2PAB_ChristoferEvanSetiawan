a6c6ad4bf7d08cb30c500a98d04d111b741c41ef
ChristoferEvan <evan200421@gmail.com>
2023-10-02 14:49:41 +0700

    Tugas1.2PAB_ChristoferEvanSetiawan

---
+++ test/tugas12christoferevansetiawan_test.dart
@@ -0,0 +1,8 @@
+import 'package:tugas12christoferevansetiawan/tugas12christoferevansetiawan.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
